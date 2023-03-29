0905bb3d8fdac4d95e6e61b7d8a70445575a24a9
Alastair Gray <alastair.gray@gladeye.com>
2023-03-29 15:19:43 +1300

    Updating code.ts to match code.js

--- code.js
+++ code.js
@@ -1,32 +1,41 @@
-figma.showUI(__html__, { width: 240, height: 340 });
-
-async function changeFont(node, fontFamily) {
-  if (node.type === "TEXT") {
-    // Load the font before applying it
-    await figma.loadFontAsync({ family: fontFamily, style: "Regular" });
-    node.fontName = { family: fontFamily, style: "Regular" };
-  }
-
-  if ("children" in node) {
-    for (const child of node.children) {
-      await changeFont(child, fontFamily);
-    }
-  }
-}
-
-figma.ui.onmessage = async (msg) => {
-  if (msg.type === "apply-font") {
-    const nodes = figma.currentPage.selection;
-    const fontFamily = msg.fontFamily;
-
-    if (nodes.length === 0) {
-      figma.notify("Please select a layer group.");
-    } else {
-      for (const node of nodes) {
-        await changeFont(node, fontFamily);
-      }
-      figma.notify("Font has been applied successfully.");
-      figma.ui.close();
-    }
-  }
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
+figma.showUI(__html__, { width: 240, height: 340 });
+function changeFont(node, fontFamily) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (node.type === "TEXT") {
+            // Load the font before applying it
+            yield figma.loadFontAsync({ family: fontFamily, style: "Regular" });
+            node.fontName = { family: fontFamily, style: "Regular" };
+        }
+        if ("children" in node) {
+            for (const child of node.children) {
+                yield changeFont(child, fontFamily);
+            }
+        }
+    });
+}
+figma.ui.onmessage = (msg) => __awaiter(void 0, void 0, void 0, function* () {
+    if (msg.type === "apply-font") {
+        const nodes = figma.currentPage.selection;
+        const fontFamily = msg.fontFamily;
+        if (nodes.length === 0) {
+            figma.notify("Please select a layer group.");
+        }
+        else {
+            for (const node of nodes) {
+                yield changeFont(node, fontFamily);
+            }
+            figma.notify("Font has been applied successfully.");
+            figma.ui.close();
+        }
+    }
+});
--- code.ts
+++ code.ts
@@ -1,33 +1,32 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
+figma.showUI(__html__, { width: 240, height: 340 });
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
-
-// This shows the HTML page in "ui.html".
-figma.showUI(__html__);
-
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage = msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+async function changeFont(node, fontFamily) {
+  if (node.type === "TEXT") {
+    // Load the font before applying it
+    await figma.loadFontAsync({ family: fontFamily, style: "Regular" });
+    node.fontName = { family: fontFamily, style: "Regular" };
   }
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+  if ("children" in node) {
+    for (const child of node.children) {
+      await changeFont(child, fontFamily);
+    }
+  }
+}
+
+figma.ui.onmessage = async (msg) => {
+  if (msg.type === "apply-font") {
+    const nodes = figma.currentPage.selection;
+    const fontFamily = msg.fontFamily;
+
+    if (nodes.length === 0) {
+      figma.notify("Please select a layer group.");
+    } else {
+      for (const node of nodes) {
+        await changeFont(node, fontFamily);
+      }
+      figma.notify("Font has been applied successfully.");
+      figma.ui.close();
+    }
+  }
 };
